76440b1e61a066b9a8c900b6383ecab354607014
abraham-murillo <abraham140600@gmail.com>
2022-03-17 19:24:01 -0600

    Tags relationship between omegaup and codeforces

--- omegaupApi.py
+++ omegaupApi.py
@@ -5,8 +5,25 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 
+import trashTags
 from pprint import pprint
 import json
+
+tagPrefixes = ['problemTag', 'problemTopic']
+
+
+def destroyCamelCase(text):
+    for prefix in tagPrefixes:
+        if text.startswith(prefix):
+            text = text[len(prefix):]
+
+    answer = ""
+    for ch in text:
+        if ch.isupper() and len(answer):
+            answer += ' '
+        answer += ch.lower()
+
+    return answer.lower()
 
 
 def getFrom(url):
@@ -57,21 +74,6 @@ def getFrom(url):
         problems = soup.find('script', attrs={'id': 'payload'})
         problems = json.loads(problems.contents[0])['problems']
 
-        tagPrefixes = ['problemTag', 'problemTopic']
-
-        def destroyCamelCase(text):
-            for prefix in tagPrefixes:
-                if text.startswith(prefix):
-                    text = text[len(prefix):]
-
-            answer = ""
-            for ch in text:
-                if ch.isupper() and len(answer):
-                    answer += ' '
-                answer += ch.lower()
-
-            return answer.lower()
-
         return [{
             'url': problem['alias'],
             'name': problem['title'],
@@ -94,6 +96,7 @@ def getAllTags():
             if line.isdigit():
                 level = int(line)
             elif len(line) > 0:
+                line = line.lower()
                 if line not in tags:
                     tags[line] = level
                     count[line] = 1
@@ -101,9 +104,15 @@ def getAllTags():
                     tags[line] += level
                     count[line] += 1
 
+    for tag, level in trashTags.tags:
+        tag = destroyCamelCase(tag).lower()
+        if tag not in tags:
+            tags[tag] = level
+            count[tag] = 1
+
     # Aproximate to codeforces rating
     # Sort tags by average rating (in theory those are easier)
-    tags = [(tag.lower(), rating * 900 / count[tag])
+    tags = [(tag.lower(), round(rating * 900 / count[tag], 2))
             for tag, rating in tags.items()]
     return sorted(tags, key=lambda x: x[1])
 
@@ -167,15 +176,43 @@ tagsRating = [('formatted input and output', 900.0),
               ('brute force', 900.0),
               ('functions', 900.0),
               ('analytic geometry', 900.0),
+              ('simulation', 900.0),
+              ('gcd and lcm', 900.0),
+              ('partial sums', 900.0),
+              ('prime factorization', 900.0),
+              ('exponential search', 900.0),
+              ('tree tranversal', 900.0),
+              ('breadth first search', 900.0),
+              ('depth first search', 900.0),
+              ('disjoint sets', 900.0),
+              ('inverted indices', 900.0),
+              ('incremental search', 900.0),
+              ('local search', 900.0),
+              ('meet in the middle', 900.0),
+              ('bipartite matching', 900.0),
+              ('divisibility rules', 1170.0),
+              ('combinatorial designs', 1440.0),
+              ('numerical series', 1440.0),
+              ('exponentiation by squaring', 1440.0),
+              ('shortest paths', 1440.0),
+              ('minimum spanning trees', 1440.0),
+              ('sub array search', 1440.0),
+              ('subsequence search', 1440.0),
+              ('monotone stack', 1440.0),
+              ('two pointers technique', 1440.0),
+              ('combinations', 1530.0),
+              ('heaps', 1530.0),
+              ('graph connectivity', 1530.0),
+              ('trees', 1530.0),
+              ('topological sorting', 1530.0),
+              ('wavelet trees', 1530.0),
               ('loops', 1800.0),
               ('arithmetic', 1800.0),
               ('conditionals', 1800.0),
               ('input and output', 1800.0),
               ('matrices', 1800.0),
-              ('omi', 1800.0),
               ('stacks', 1800.0),
               ('queues', 1800.0),
-              ('disjoint sets', 1800.0),
               ('binary search tree', 1800.0),
               ('tree traversal', 1800.0),
               ('divide and conquer', 1800.0),
@@ -192,9 +229,31 @@ tagsRating = [('formatted input and output', 900.0),
               ('prime generation', 1800.0),
               ('counting problems', 1800.0),
               ('big numbers', 1800.0),
+              ('systems of equations', 1800.0),
+              ('diophantine equations', 1800.0),
+              ('modular multiplicative inverse', 1800.0),
+              ('chinese remainder theorem', 1800.0),
+              ('permutations', 1800.0),
+              ('game theory', 1800.0),
+              ('probability and statistics', 1800.0),
+              ('string matching', 1800.0),
+              ('linked lists', 1800.0),
+              ('directed graphs', 1800.0),
+              ('graphs with negative weights', 1800.0),
+              ('tries', 1800.0),
+              ('least common ancestor', 1800.0),
+              ('offline queries', 1800.0),
+              ('sqrt decomposition', 1800.0),
+              ('nearest neighbors', 1800.0),
               ('chars and strings', 1950.0),
               ('arrays', 1980.0),
               ('sorting', 1980.0),
+              ('fourier transformation', 1980.0),
+              ('boolean algebra', 1980.0),
+              ('lazy propagation', 1980.0),
+              ('palindrome algorithms', 1980.0),
+              ('convex hull', 1980.0),
+              ('sweep line', 1980.0),
               ('implementation', 2025.0),
               ('binary search', 2025.0),
               ('dynamic programming', 2250.0),
@@ -203,11 +262,15 @@ tagsRating = [('formatted input and output', 900.0),
               ('graph representation', 2250.0),
               ('hashing', 2250.0),
               ('bit manipulation', 2250.0),
+              ('max flow', 2430.0),
+              ('suffix trees', 2430.0),
               ('segment trees', 2700.0),
               ('fenwick trees', 2700.0),
               ('big data', 2700.0),
               ('unformatted input and output', 2700.0),
-              #   ('file seeking', 2700.0),
+              ('file seeking', 2700.0),
               ('data compression', 2700.0),
               ('lexing and parsing', 2700.0),
-              ('heuristics', 2700.0)]
+              ('heuristics', 2700.0),
+              ('genetic algorithms', 3150.0),
+              ('particle swarm optimization', 3150.0)]
