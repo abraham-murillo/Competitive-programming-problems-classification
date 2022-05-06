a7ab829ce649365b3103f56d699d1acebf2f0e05
burinson <garentei@gmail.com>
2022-05-06 14:04:47 -0500

    Added search design css

--- web-page/src/Navbar.jsx
+++ web-page/src/Navbar.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-import { Box, Spacer, Flex, Image, Text, Container } from "@chakra-ui/react";
+import {
+  Button,
+  ButtonGroup,
+  Input,
+  Box,
+  Spacer,
+  Flex,
+  Container,
+} from "@chakra-ui/react";
+import { BiCodeCurly, BiCodeBlock } from "react-icons/bi";
 
 export default function Navbar() {
   return (
     <Box boxShadow={"md"}>
       <Container>
-        <Flex align={"center"} height={"38px"}>
-          <Box w={"86px"}>
-            {/* <Image w={"100px"} src={logo} /> */}
-          </Box>
-
-          <Text> Competitive programming problem clasificator </Text>
+        <Flex align={"center"} height={"80px"}>
+          <Box w={"86px"}>{/* <Image w={"100px"} src={logo} /> */}</Box>
+          <Input placeholder="Título del problema" />
+          <Button marginLeft={3} colorScheme="teal" size="md">
+            Buscar
+          </Button>
           <Spacer />
-
         </Flex>
       </Container>
     </Box>
   );
-};
-
+}
