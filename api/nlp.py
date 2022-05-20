77663c3679eb5d391aa6378c45456896274966fa
burinson <garentei@gmail.com>
2022-05-20 13:30:28 -0500

    Función de Python para filtrado (lemmatización, lowercase, stopwords) xd

--- api/nlp.py
+++ api/nlp.py
@@ -2,6 +2,7 @@ import spacy
 from pprint import pprint
 import json
 
+
 class SmartNLP():
     # TODO: Enable both english and spanish nlp
     english = spacy.load("en_core_web_sm")
@@ -11,7 +12,9 @@ class SmartNLP():
         # Returns nlp based on the language
         return self.english
 
+
 smart = SmartNLP()
+
 
 def tokenize(text):
     nlp = smart.detectLanguage(text)
@@ -30,5 +33,23 @@ def tokenize(text):
 
     return list(tokens.values())
 
-text = "hello, how are you?"
-# pprint(tokenize(text))
+
+def lemmatize(text):
+    nlp = smart.detectLanguage(text)
+    text = nlp(text)
+    lemmatizedText = str()
+
+    for word in text:
+        if not word.is_punct and not word.is_stop:
+            lemmatizedText += word.lemma_.lower() + " "
+
+    return lemmatizedText[:-1]
+
+
+def filterText(text):
+    lemmatizedText = lemmatize(text)
+    return lemmatizedText
+
+
+text = "I am the green child. Behold! a green child is here! Denounce the all-mighty taurus and begone my young ones!"
+pprint(lemmatize(text))
