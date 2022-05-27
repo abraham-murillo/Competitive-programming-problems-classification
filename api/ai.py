6e5a2295f7e5f48109cec7c84d5c1a8013b9c79c
burinson <garentei@gmail.com>
2022-05-26 19:15:46 -0500

    Dummy AI

--- api/ai.py
+++ api/ai.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from api import getAll
 from api import nlp
 # import pandas as pd
-# import numpy as np
+import numpy as np
 from numpy import asarray
 # from numpy import zeros
 from pprint import pprint
@@ -23,8 +23,25 @@ import matplotlib.pyplot as plt
 
 
 def train():
-    pprint("Training model...")
-    allProblems = getAll()[:10]
+    pprint("Starting model training...")
+    # allProblems = getAll()[:10]
+    allProblems = [
+        {
+            "history": "history1",
+            "topics": [
+                {"id": "id1"}
+            ]
+        }, {
+            "history": "history2",
+            "topics": [
+                {"id": "id2"}
+            ]
+        }, {
+            "history": "history3",
+            "topics": [
+                {"id": "id3"}
+            ]}
+    ]
     pprint(allProblems)
     X = []
 
@@ -47,52 +64,58 @@ def train():
 
     pprint(Y[:10])
 
-    # X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20)
 
-    # tokenizer = Tokenizer(num_words=5000)
-    # tokenizer.fit_on_texts(X_train)
-    # X_train = tokenizer.texts_to_sequences(X_train)
-    # X_test = tokenizer.texts_to_sequences(X_test)
+    tokenizer = Tokenizer(num_words=5000)
+    tokenizer.fit_on_texts(X_train)
+    X_train = tokenizer.texts_to_sequences(X_train)
+    X_test = tokenizer.texts_to_sequences(X_test)
 
-    # maxLen = 100
+    maxLen = 100
 
-    # X_train = pad_sequences(X_train, padding='post', maxlen=maxLen)
-    # X_test = pad_sequences(X_test, padding='post', maxlen=maxLen)
+    X_train = pad_sequences(X_train, padding='post', maxlen=maxLen)
+    X_test = pad_sequences(X_test, padding='post', maxlen=maxLen)
 
-    # # # TODO: Find a place to store Word2Vect and use it here
-    # embeddings = dict()
-    # embeddingsFile = open("/home/uriel/CUCEI/Word2VecEnglish.txt", "r")
+    # # TODO: Find a place to store Word2Vect and use it here
+    embeddings = dict()
+    embeddingsFile = open("/home/uriel/CUCEI/Word2VecEnglish.txt", "r")
+    it = 0
 
-    # for line in embeddingsFile:
-    #     features = line.split()
-    #     word = features[0]
-    #     vector = asarray(features[1:], dtype='float32')
-    #     embeddings[word] = vector
+    for line in embeddingsFile:
+        # TODO: Remove this line when we have a reasonable GPU to train the model with. This is just for testing.
+        if it == 100:
+            break
 
-    # embeddingsFile.close()
-    # vocabSize = len(tokenizer.word_index) + 1
-    # embeddingMatrix = zeros((vocabSize, 300))
+        it += 1
+        features = line.split()
+        word = features[0]
+        vector = asarray(features[1:], dtype='float32')
+        embeddings[word] = vector
 
-    # for word, index in tokenizer.word_index.items():
-    #     embeddingVector = embeddings.get(word)
+    embeddingsFile.close()
+    vocabSize = len(tokenizer.word_index) + 1
+    embeddingMatrix = np.zeros((vocabSize, 300))
 
-    #     if embeddingVector is not None:
-    #         embeddingMatrix[index] = embeddingVector
+    for word, index in tokenizer.word_index.items():
+        embeddingVector = embeddings.get(word)
 
-    # model = Sequential()
-    # embeddingLayer = Embedding(vocabSize, 300, weights=[
-    #                            embeddingMatrix], input_length=maxLen, trainable=False)
-    # model.add(embeddingLayer)
-    # model.add(Flatten())
-    # model.add(Dense(1, activation='sigmoid'))
-    # model.compile(optimizer='adam',
-    #               loss='binary_crossentropy', metrics=['acc'])
+        if embeddingVector is not None:
+            embeddingMatrix[index] = embeddingVector
 
-    # history = model.fit(X_train, np.array(Y_train), batch_size=128,
-    #                     epochs=10, verbose=1, validation_split=0.2)
-    # score = model.evaluate(X_test, np.array(Y_test), verbose=1)
-    # print("Test Loss:", score[0])
-    # print("Test Accuracy:", score[1])
+    model = Sequential()
+    embeddingLayer = Embedding(vocabSize, 300, weights=[
+                               embeddingMatrix], input_length=maxLen, trainable=False)
+    model.add(embeddingLayer)
+    model.add(Flatten())
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy', metrics=['acc'])
+
+    history = model.fit(X_train, np.array(Y_train), batch_size=128,
+                        epochs=10, verbose=1, validation_split=0.2)
+    score = model.evaluate(X_test, np.array(Y_test), verbose=1)
+    print("Test Loss:", score[0])
+    print("Test Accuracy:", score[1])
 
 
 train()
